04d9725001454ae7dd9e1517c1dde118551bbdc6
alu20470789f <alu20470789f@ieselcaminas.org>
2018-09-25 17:42:55 +0200

    Avoid overwrite buttons

--- app/src/main/java/pmdm/ieselcaminas/org/tictac_resolution/MainActivity.java
+++ app/src/main/java/pmdm/ieselcaminas/org/tictac_resolution/MainActivity.java
@@ -60,9 +60,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Button button = (Button) v;
-                button.setText(currentPlayer.symbol);
-                changePlayer();
-                displayTurn();
+
+                if ( button.getText().equals("")) {
+                    button.setText(currentPlayer.symbol);
+                    changePlayer();
+                    displayTurn();
+                }
             }
         });
     }
